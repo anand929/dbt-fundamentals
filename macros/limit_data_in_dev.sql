36ed154b19bc9b4f37896223056ea83350518893
erad929@fastservice.com <erad929@fastservice.com>
2022-08-26 07:16:20 +0000

    Corrected limit_data_in_dev.sql

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,5 +1,5 @@
 {% macro limit_data_in_dev(env_name='default') -%}
 {%- if target.name == env_name -%}
-where created_at >= dateadd('day',-3,current_timestamp)
+where created >= CURRENT_DATE + INTERVAL '3 day'--dateadd('day',-3,current_timestamp)
 {%- endif -%}
 {%- endmacro %}
