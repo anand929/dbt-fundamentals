c019c45b715a84a9dda029cfcf9ba6f55c55dafc
erad929@fastservice.com <erad929@fastservice.com>
2024-01-17 19:22:27 +0000

    target variable example

---
+++ models/target_v/target_variable.sql
@@ -0,0 +1,9 @@
+select '{{ target.profile_name}}' as profilename,
+'{{ target.name}}' as targetname,
+'{{ target.schema}}' as targetschema,
+'{{ target.type}}' as targettype,
+'{{ target.threads}}' as threads,
+'{{ target.dbname}}' as dbname,
+'{{ target.host}}' as hostname,
+'{{ target.port}}' as portnumber,
+'{{ target.user}}' as username
