68bf1b70a1c8a00c6bf784657684497a93bd5bac
erad929@fastservice.com <erad929@fastservice.com>
2024-02-01 11:40:32 +0000

    on_schema_change different strategies for incremental materialization

--- models/marts/incr/stg_customers_incr.sql
+++ models/marts/incr/stg_customers_incr.sql
@@ -1,6 +1,18 @@
+{#
+1. If not unique key defined, dbt will append the records with insert script
+2. If unique key defined, dbt will upsert the records with merge script
+3. By default, new columns added to upper layer will not be picked up by dbt.
+4. on_schema_change='ignore', any schema change will be ignored.
+5. on_schema_change='fail', The source and target schemas on this incremental model are out of sync!
+6. on_schema_change='append_new_columns', alter target table add new columns
+7. on_schema_change='sync_all_columns', alter target table add/drop columns wrt source
+   Note:None of the on_schema_change behaviors backfill values in old records for newly added columns.
+#}
+
 {{
-    config(
-        unique_key='id'
+    config(       
+        unique_key='id',
+        on_schema_change='ignore'        
     )
 
 }}
